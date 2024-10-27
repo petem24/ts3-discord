95da5ea95297b87d44c1a02bb4a67b9a6e8c179e
Peter Maher <pete.maher2@gmail.com>
2024-10-27 13:57:42 +0000

    Cleanup

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 import { QueryProtocol, TeamSpeak } from "npm:ts3-nodejs-library";
-import { Client, REST, Routes, TextChannel } from "npm:discord.js";
+import { Client, TextChannel } from "npm:discord.js";
 
 /**
  * Teamspeak setup
@@ -28,28 +28,8 @@ const discord = new Client({
   intents: ["GuildMessages", "Guilds", "MessageContent"],
 });
 
-const rest = new REST({ version: "10" }).setToken(
-  Deno.env.get("DISCORD_TOKEN") as string,
-);
-
 discord.on("ready", () => {
   console.log("Discord instance set");
-
-  // try {
-  //   await registerSlashCommands();
-  //   console.log("Discord slash commands registered");
-  // } catch (err) {
-  //   console.error(err);
-  //   console.log("Unable to register slash commands");
-  // }
-
-  // try {
-  //   registerSlashCommandListeners();
-  //   console.log("Discord commands listening");
-  // } catch (err) {
-  //   console.error(err);
-  //   console.log("Unable to register slash commands listeners");
-  // }
 });
 
 discord.on("error", (e) => {
@@ -59,71 +39,8 @@ discord.on("error", (e) => {
 
 discord.login(Deno.env.get("DISCORD_TOKEN"));
 
-// const registerSlashCommands = async () => {
-//   const commands = [
-//     {
-//       name: "setup",
-//       description: "Used to setup the bot",
-//     },
-//   ];
-
-//   await rest.put(Routes.applicationCommands("1299810659718004766"), {
-//     body: commands,
-//   });
-// };
-
-// const registerSlashCommandListeners = () => {
-//   discord.on(Events.InteractionCreate, (interaction) => {
-//     if (!interaction.isChatInputCommand()) return;
-
-//     switch (interaction.commandName) {
-//       case "setup": {
-//         try {
-//           if (!interaction.channel) {
-//             throw new Error();
-//           }
-
-//           interaction.reply({ content: "Setting up..." });
-//           break;
-//         } catch (e) {
-//           console.error(e);
-//           interaction.editReply({
-//             content: "Error setting up...",
-//           });
-//         }
-//       }
-//     }
-
-//     // if (interaction.commandName === "ping") {
-//     //   await interaction.reply({ content: "Secret Pong!", ephemeral: true });
-//     // }
-//   });
-// };
-
-const getMessageContent = async () => {
-  const data = await getChannelData();
-
-  const lastUpdated = new Date();
-  const formattedDate = lastUpdated.toLocaleString(); // Customize format as needed
-
-  const header = `**Last Updated:** ${formattedDate}\n\n`;
-
-  return header + data.map((channel) => {
-    const channelInfo = `**${channel.name}**`;
-    const clientsInfo = channel.clients.length > 0
-      ? channel.clients.map((client) =>
-        `- **${client.name}**${client.inputMuted ? " 🔇 Input Muted" : ""}${
-          client.outputMuted ? " 🔈 Output Muted" : ""
-        }`
-      ).join("\n")
-      : "No clients connected.";
-
-    return `${channelInfo}\n${clientsInfo}`;
-  }).join("\n\n");
-};
-
 /**
- * Web Server setup
+ * Web Server setup (this was for development only)
  */
 Deno.serve(async (_req) => {
   const channelListRes = await channelList(_req);
@@ -161,25 +78,6 @@ const updateMessage = async (req: Request): Promise<Response | undefined> => {
   return new Response();
 };
 
-const getChannelData = async () => {
-  const channels = await ts3.channelList();
-
-  return await Promise.all<IChannelData>(channels.map(async (x) => {
-    return {
-      id: x.cid,
-      name: x.name,
-      clients: (await x.getClients({ clientType: 0 })).map((y) => {
-        return {
-          id: y.clid,
-          name: y.nickname,
-          inputMuted: y.inputMuted,
-          outputMuted: y.outputMuted,
-        };
-      }),
-    };
-  }));
-};
-
 /**
  * Cron setup
  */
@@ -203,6 +101,50 @@ const onCron = async () => {
   } else {
     await channel.send(content);
   }
+};
+
+/**
+ * Random functions
+ */
+const getChannelData = async () => {
+  const channels = await ts3.channelList();
+
+  return await Promise.all<IChannelData>(channels.map(async (x) => {
+    return {
+      id: x.cid,
+      name: x.name,
+      clients: (await x.getClients({ clientType: 0 })).map((y) => {
+        return {
+          id: y.clid,
+          name: y.nickname,
+          inputMuted: y.inputMuted,
+          outputMuted: y.outputMuted,
+        };
+      }),
+    };
+  }));
+};
+
+const getMessageContent = async () => {
+  const data = await getChannelData();
+
+  const lastUpdated = new Date();
+  const formattedDate = lastUpdated.toLocaleString(); // Customize format as needed
+
+  const header = `**Last Updated:** ${formattedDate}\n\n`;
+
+  return header + data.map((channel) => {
+    const channelInfo = `**${channel.name}**`;
+    const clientsInfo = channel.clients.length > 0
+      ? channel.clients.map((client) =>
+        `- **${client.name}**${client.inputMuted ? " 🔇 Input Muted" : ""}${
+          client.outputMuted ? " 🔈 Output Muted" : ""
+        }`
+      ).join("\n")
+      : "No clients connected.";
+
+    return `${channelInfo}\n${clientsInfo}`;
+  }).join("\n\n");
 };
 
 export interface IChannelData {
